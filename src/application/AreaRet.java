4164919e0b0978eae81413bfd13d7700db8dc4a5
Cristiano Muroni <muronislick@hotmail.com>
2020-03-09 00:44:37 -0300

    incrementando a classe: 'Rectangle'

--- src/application/AreaRet.java
+++ src/application/AreaRet.java
@@ -2,26 +2,23 @@ package application;
 
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Rectangle;
 
+import entities.Rectangle;
 
 public class AreaRet {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		Rectangle rectangle = new Rectangle();
-		
-		
-		
+
 		System.out.println("entre com as dimensões do retângulo");
 		System.out.println("digite o valor da altura:");
 		rectangle.alt = sc.nextDouble();
 		System.out.println("digite um valor para largura:");
-		rectangle.larg =sc.nextDouble();
+		rectangle.larg = sc.nextDouble();
 		System.out.println();
 		System.out.println(rectangle);
-		
 
 	}
 
